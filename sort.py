c47b8b99d528500a50748b54b6fad6e47f3394a3
Kushal Matalia <kushalmatalia15.12@gmail.com>
2023-09-23 08:22:17 +0530

    Adding Code for Bubble Sort with the test data -v1

--- sort.py
+++ sort.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+import time
+import random
 
 """Module with the base implementation of a Sort class."""
 
@@ -21,5 +23,35 @@ class Sort(ABC):
         return self._items
 
     def _time(self):
-        self.time = 0
-        return self.time
+        start_time = time.time()
+        expected_output = self._sort()
+        # print("Sorted Array :: ", expected_output)
+        end_time = time.time()
+        return end_time - start_time
+    
+class BubbleSort(Sort):
+
+  def __init__(self, arr):
+    self.arr = arr
+
+  def _sort(self):
+    n = len(self.arr)
+    for i in range(n):
+      for j in range(0, n-i-1):
+        if self.arr[j] > self.arr[j+1]:
+          self.arr[j], self.arr[j+1] = self.arr[j+1], self.arr[j]
+    return self.arr
+
+
+if __name__ == "__main__":
+  data_sizes = [50, 100, 500, 1000, 2000, 3000, 4000, 5000]
+  bubble_sort_times = []
+
+  for size in data_sizes:
+    data = [random.randint(1, 100) for i in range(size)]
+    bub_sorting = BubbleSort(data)
+    bub_execution_time = bub_sorting._time()
+    # print(f"Bubble Sorting time {size} :", bub_execution_time)
+    bubble_sort_times.append(bub_execution_time)
+
+  print("Bubble Sort timings :: ", bubble_sort_times)
